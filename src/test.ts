40b412a73e28dbea4bbf038d3f6ca1d6ba9d7ae8
lif9e1 <31802212+lif9e1@users.noreply.github.com>
2019-10-26 16:52:32 +0200

    deleted some comments for cleaner structure

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,2 @@
 import { agent } from './index';
-
-// (async () => {
-//   let res = await agent.get(
-//     'https://srh.westeurope.cloudapp.azure.com/migration/recipes2'
-//   );
-//   console.log(JSON.stringify(res, null, 2));
-// })();
-
 agent.get('https://www.google.com/test1234').then(res => console.log(res));
